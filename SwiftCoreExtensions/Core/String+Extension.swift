23d844e463bedea6759c8631b7d70c5a48bef634
Jared Friese <jared.friese@gmail.com>
2016-09-07 13:42:09 -0700

    Run trim command on codebase

--- SwiftCoreExtensions/Core/String+Extension.swift
+++ SwiftCoreExtensions/Core/String+Extension.swift
@@ -4,28 +4,28 @@ extension String {
     public func splitAll(string: String) -> [String] {
         return self.componentsSeparatedByString(string)
     }
-    
+
     public func split(string: String, times: Int) -> [String] {
         if times < 0 {
             return [self]
         }
-        
+
         let allComponents = self.splitAll(string)
         if times >= allComponents.count {
             return allComponents
         }
-        
+
         var components = [String]()
         for i in 0..<times {
             components.append(allComponents[i])
         }
-        
+
         let numRemainingComponents = allComponents.count - times
         var finalComponent = allComponents[times]
         for i in (0..<(numRemainingComponents-1)).reverse() {
             finalComponent = finalComponent + string + allComponents[allComponents.count - i - 1]
         }
-        
+
         components.append(finalComponent)
         return components
     }
--- SwiftCoreExtensionsTests/Core/String+ExtensionSpec.swift
+++ SwiftCoreExtensionsTests/Core/String+ExtensionSpec.swift
@@ -7,27 +7,27 @@ class String_ExtensionSpec: XCTestCase {
         let components = "t:h:i:s".splitAll(":")
         expect(components).to(equal(["t", "h", "i", "s"]))
     }
-    
+
     func test_splitAll_whenTheGivenInputDoesNotAppear_returnsArrayContainingEntireString() {
         let components = "t:h:i:s".splitAll(",")
         expect(components).to(equal(["t:h:i:s"]))
     }
-    
+
     func test_splitTimes_splitsTheStringUpToNTimes() {
         let components = "t:h:i:s".split(":", times: 2)
         expect(components).to(equal(["t", "h", "i:s"]))
     }
-    
+
     func test_splitTimes_whenPassedNegativeValueForTimes_returnsArrayContainingEntireString() {
         let components = "t:h:i:s".split(":", times: -1)
         expect(components).to(equal(["t:h:i:s"]))
     }
-    
+
     func test_splitTimes_whenPassedZeroForTimes_returnsArrayContainingEntireString() {
         let components = "t:h:i:s".split(":", times: 0)
         expect(components).to(equal(["t:h:i:s"]))
     }
-    
+
     func test_splitTimes_whenPassedValueForTimesGreaterThanPossibleSplitComponentCount_splitsTheStringAsManyTimesAsItCan() {
         let components = "t:h:i:s".split(":", times: 1000)
         expect(components).to(equal(["t", "h", "i", "s"]))
